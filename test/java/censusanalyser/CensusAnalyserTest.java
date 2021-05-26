ffaf979cc7e08bf5c008db1628d97b61d32d6ad7
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-27 00:44:37 +0530

    Added: custom exception for wrong path

--- test/java/censusanalyser/CensusAnalyserTest.java
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -2,10 +2,12 @@ package censusanalyser;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     //checking the number of entries
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -14,5 +16,17 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
         } catch (CensusAnalyserException e) { }
+    }
+    //for custom exception 1 wrong path
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
     }
 }
