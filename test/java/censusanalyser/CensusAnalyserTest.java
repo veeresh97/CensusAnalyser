2ec2d388ca74ff7c080baec1db22734d5418284d
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-27 00:30:29 +0530

    Added: census analyser code

---
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,18 @@
+package censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+}
